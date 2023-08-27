12ed6addf58c28de6517cf8575299d0336d1206b
Victoria Kalenik <vk781@hotmail.com>
2023-08-27 22:22:09 +0000

    End of lesson

--- reduce.js
+++ reduce.js
@@ -1,11 +1,27 @@
-  
+
 /**
  * To run this file in Gitpod, use the 
  * command node reduce.js in the terminal
  */
 
+// // Ditailed 
+// // Summing an array of numbers:
+// const nums = [0, 1, 2, 3, 4];
+// let sum = nums.reduce((acc, curr) => {
+//   console.log(
+//     "Accumulator:", acc,
+//     "Current Value:", curr,
+//     "Total:", acc + curr
+//   );
+//   return acc + curr;
+// }, 10);
+// console.log(sum);
 
+// 'One-liner'
 // Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc + curr, 0);
+console.log(sum);
 
 
 const teamMembers = [
@@ -28,10 +44,27 @@ const teamMembers = [
     name: 'Kelly',
     profession: 'Designer',
     yrsExperience: 3
+  },
+  {
+    name: 'Mark',
+    profession: 'Manger',
+    yrsExperience: 10
   }
 ];
 
 // Totaling a specific object property
-
+let totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+console.log(totalExperience);
 
 // Grouping by a property, and totaling it too
+// {Developer: 12, Designer; 4} <-- this is what we want!
+let experienceByProfession = teamMembers.reduce((acc, curr) => {
+  let key = curr.profession;
+  if (!acc[key]) {
+    acc[key] = curr.yrsExperience;
+  } else {
+    acc[key] += curr.yrsExperience;
+  }
+  return acc;
+}, {});
+console.log(experienceByProfession);
